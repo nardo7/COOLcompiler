3c654aae09f27e67a460026c2b6d9b586d0f70d7
Nardo <alejandronrd0@gmail.com>
2020-01-08 15:21:34 +0100

    some unnecessary things removed

--- MIPSCodeGenerator/MIPSCodeGenerator/Helper.cs
+++ MIPSCodeGenerator/MIPSCodeGenerator/Helper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MIPSCodeGenerator
+﻿namespace MIPSCodeGenerator
 {
     public static class Helper
     {
